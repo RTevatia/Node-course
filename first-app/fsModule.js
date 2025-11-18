f7ce6ac3cf4cfab1a5bb9f320948468c6f655e75
Rahul <rtevatia2@gmail.com>
2025-11-17 20:32:56 -0500

    docs(fs): add file system module demonstration

---
+++ first-app/fsModule.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+// Synchronous 
+// const files = fs.readdirSync("./");
+// console.log(files);
+
+// Asynchronus
+fs.readdir('./', function(err, files) {
+    if (err) console.log('Error: ' + err);
+    else console.log('Result: ' + files);  
+});

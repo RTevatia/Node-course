9a5e892ea3ad46d244482d179cac00a4165cc230
Rahul <rtevatia2@gmail.com>
2025-11-19 07:28:23 -0500

    feat(api): add route and query param handling
    -Implements endpoints using route parameters and query parameters.
    -Enables fetching single resources and filtered collections.
    -Improves API flexibility and clarity.

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,5 +9,20 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
+// single route parameter - in this case id
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+// muliple route parameter - in this case year and month
+app.get("/api/courses/:year/:month", (req, res) => {
+  res.send(req.params);
+});
+
+// query string parameter
+app.get("/api/courses/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));

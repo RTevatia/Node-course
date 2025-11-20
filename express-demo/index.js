074acc6943799e4bae7ab86694cee987bd26302c
Rahul <rtevatia2@gmail.com>
2025-11-19 23:19:15 -0500

    feat(put): add PUT for updating the course with id

--- express-demo/index.js
+++ express-demo/index.js
@@ -19,28 +19,40 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  const { error, value } = schema.validate(req.body);
+  const { error, value } = validateCourse(req.body);
 
   if (error) {
     res.status(400).send(error.details[0].message);
     return;
   }
 
-  //   if (!req.body.name || req.body.name.length < 3) {
-  //     // 400 Bad Request
-  //     res.status(400).send("Name is required and should be minimum 3 characters");
-  //     return;
-  //   }
-
   const course = {
     id: courses.length + 1,
     name: req.body.name,
   };
   courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  // Look up the course
+  // If not existing, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found!");
+
+  // Validate
+  // If invalid, return 400 - Bad request
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  // Update course
+  // Return the updated course
+  course.name = req.body.name;
   res.send(course);
 });
 
@@ -53,3 +65,12 @@ app.get("/api/courses/:id", (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}

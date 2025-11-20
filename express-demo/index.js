31243dd79f67812e2a0a2a3aef5874e670d4864b
Rahul <rtevatia2@gmail.com>
2025-11-19 22:25:14 -0500

    feat(validation): add Joi schema-based input check

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,5 +1,7 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
+
 app.use(express.json());
 
 const courses = [
@@ -17,6 +19,23 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  const { error, value } = schema.validate(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  //   if (!req.body.name || req.body.name.length < 3) {
+  //     // 400 Bad Request
+  //     res.status(400).send("Name is required and should be minimum 3 characters");
+  //     return;
+  //   }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,

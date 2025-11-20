99357ea5e4b5caa815ee76e145bad840d29be62c
Rahul <rtevatia2@gmail.com>
2025-11-19 21:48:26 -0500

    feat(api): add POST endpoint for course creation

--- express-demo/index.js
+++ express-demo/index.js
@@ -13,20 +13,20 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-app.post('/api/courses', (req, res) => {
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-})
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
 
 app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
     return res.status(404).send("The course with the given ID was not found!");
   res.send(course);

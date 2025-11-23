407b8dc4c5236d7498baaa3df4f358a0725b385a
Rahul <rtevatia2@gmail.com>
2025-11-23 17:03:38 -0500

    docs(express): explain built-in middleware usage

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,12 +2,14 @@ const logger = require('./logger');
 const authenticate = require('./authenticate');
 const Joi = require("joi");
 const express = require("express");
+
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
 
 app.use(logger);
-
 app.use(authenticate);
 
 const courses = [

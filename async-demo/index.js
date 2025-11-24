77bd580d3346d083a1d75c8a10490fe782665cf5
Rahul <rtevatia2@gmail.com>
2025-11-24 13:23:00 -0500

    docs(async): add callback pattern example

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,24 @@
 console.log("Before");
-setTimeout(() => {
-  console.log("Reading a user from database...");
-}, 2000);
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log("Repos: ", repos);
+  });
+});
 console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading a user from database...");
+    callback({
+      id: id,
+      gitHubUsername: "Mosh",
+    });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    callback(["Repo1", "Repo2", "Repo3"]);
+  }, 2000);
+}

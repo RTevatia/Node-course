27e356ae6119652928fddcd4ce43f7989a38c6d4
Rahul <rtevatia2@gmail.com>
2025-11-23 22:34:33 -0500

    docs(config): add multi-environment config examples
    -default and env-specific config files.
    -secure mapping via env vars

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const logger = require("./logger");
@@ -7,16 +8,22 @@ const express = require("express");
 
 const app = express();
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
-console.log(app.get('env'));
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
+// console.log(app.get('env'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
-if (app.get('env') === 'development') {
-    app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
+
+// Configuration
+console.log(`Application name: ${config.get("name")}`);
+console.log(`Mail server: ${config.get("mail.host")}`);
+console.log(`Mail password: ${config.get("mail.password")}`);
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled...");
 }
 
 app.use(logger);

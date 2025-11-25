ab1bd34014a6b5ab27e00322a1aee4fde6e4dabf
Rahul <rtevatia2@gmail.com>
2025-11-24 22:03:28 -0500

    refactor(async): rewrite callback chain using promises
    -Replace nested callback structure with Promise chaining.
    -Improve readability and centralizes error handling with catch.

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,24 +1,34 @@
 console.log("Before");
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    console.log("Repos: ", repos);
-  });
-});
+getUser(1)
+    .then(user => { return getRepositories(user.gitHubUsername) })
+    .then(repos => { return getCommits(repos[0])})
+    .then(commits => { console.log('Commit', commits)})
+    .catch(err => console.log('Error', err.message));
 console.log("After");
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("Reading a user from database...");
-    callback({
-      id: id,
-      gitHubUsername: "Mosh",
-    });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading a user from a database...");
+      resolve({ id: id, gitHubUsername: "mosh" });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log("Calling GitHub API...");
-    callback(["Repo1", "Repo2", "Repo3"]);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...");
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...");
+      resolve(["commit"]);
+    }, 2000);
+  });
 }

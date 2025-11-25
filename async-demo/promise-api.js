15c9599b9eaf40bc1ee35e0e4b6e23f0428da627
Rahul <rtevatia2@gmail.com>
2025-11-24 22:32:47 -0500

    docs(promises): explain parallel async ops with examples

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,6 +1,23 @@
-const p = Promise.resolve({ id: 1 });
-p.then((result) => console.log(result));
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Asynchronous operation 1...");
+    resolve(1);
+  });
+});
 
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Asynchronous operation 2...");
+    resolve(2);
+  });
+});
 
-const p1 = Promise.reject(new Error('reason for rejection...'));
-p1.catch(error => console.log(error));
+// Promise.all example
+// Promise.all([p1, p2])
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log("Error", err));
+
+// Promise.race example
+Promise.race([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => console.log("Error", err));

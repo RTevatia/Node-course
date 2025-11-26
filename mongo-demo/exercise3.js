913641420d617f24300ac00c50a3613d43de0770
Rahul <rtevatia2@gmail.com>
2025-11-25 20:41:41 -0500

    docs(exercise3): add solution for price/title filtering

---
+++ mongo-demo/exercise3.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/mongo-exercises")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: Date,
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function getCourses() {
+  return await Course.find({ isPublished: true })
+    .or([{ price: { $gte: 15 } }, { name: /.*by.*/i }])
+    .sort("-price")
+    .select("name price");
+}
+
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+
+run();

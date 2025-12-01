176b4d675956f4de69442936d413a0688ec52cc1
Rahul <rtevatia2@gmail.com>
2025-11-30 19:03:26 -0500

    feat(embedding): add embedded author subdocument

--- mongo-demo/embedding.js
+++ mongo-demo/embedding.js
@@ -1,34 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const authorSchema = new mongoose.Schema({
   name: String,
   bio: String,
-  website: String
+  website: String,
 });
 
-const Author = mongoose.model('Author', authorSchema);
+const Author = mongoose.model("Author", authorSchema);
 
-const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String
-}));
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+    author: authorSchema,
+  })
+);
 
 async function createCourse(name, author) {
   const course = new Course({
-    name, 
-    author
-  }); 
-  
+    name,
+    author,
+  });
+
   const result = await course.save();
   console.log(result);
 }
 
-async function listCourses() { 
+async function listCourses() {
   const courses = await Course.find();
   console.log(courses);
 }
 
-createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function updateAuthor(courseID) {
+  const course = await Course.updateOne(
+    { _id: courseID }, 
+    { $unset: {"author.name": ""} });
+}
+
+// createCourse("Node Course", new Author({ name: "Mosh" }));
+updateAuthor("692cd8198c4887a93cbc1686");
+// listCourses();

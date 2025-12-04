ba0ca8175680f177a7c744fca0a865d35fb95c73
Rahul <rtevatia2@gmail.com>
2025-12-03 22:36:29 -0500

    refactor(user): move jwt logic into userSchema in user.js

--- vidly/models/user.js
+++ vidly/models/user.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const mongoose = require("mongoose");
 const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
@@ -23,6 +25,11 @@ const userSchema = mongoose.Schema({
     maxlength: 1024,
   },
 });
+
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  return token;
+}
 
 const User = mongoose.model("User", userSchema);
 
--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -1,5 +1,3 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { User } = require("../models/user");
@@ -19,7 +17,7 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) res.status(400).send("Invalid email or password.");
 
-  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+  const token = user.generateAuthToken();
   res.send(token);
 });
 

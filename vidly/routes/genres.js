fa4273494792ac9f3456415228fbd5abbba5c91e
Rahul <rtevatia2@gmail.com>
2025-12-04 20:29:00 -0500

    feat(auth): apply auth middleware to protected routes

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const { Genre, validate } = require("../models/genre");
 const mongoose = require("mongoose");
 const express = require("express");
@@ -16,8 +17,8 @@ router.get("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.post("/", async (req, res) => {
-  const { error, value } = validate(req.body);
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = new Genre({
@@ -29,8 +30,8 @@ router.post("/", async (req, res) => {
   res.send(genre);
 });
 
-router.put("/:id", async (req, res) => {
-  const { error, value } = validate(req.body);
+router.put("/:id", auth, async (req, res) => {
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(
@@ -44,7 +45,7 @@ router.put("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   const genre = await Genre.findByIdAndDelete(req.params.id);
   if (!genre)
     return res.status(404).send("The genre with given ID was not found!");
--- vidly/routes/movies.js
+++ vidly/routes/movies.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const { Movie, validate } = require("../models/movie");
 const { Genre } = require("../models/genre");
 const mongoose = require("mongoose");
@@ -17,7 +18,7 @@ router.get("/:id", async (req, res) => {
   res.send(movie);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -39,7 +40,7 @@ router.post("/", async (req, res) => {
   res.send(movie);
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -65,7 +66,7 @@ router.put("/:id", async (req, res) => {
   res.send(movie);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   const movie = await Movie.findByIdAndDelete(req.params.id);
 
   if (!movie)

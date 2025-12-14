001f3217f5c8f1f211352334c9e16f1fbca25f1b
Rahul <rtevatia2@gmail.com>
2025-12-14 12:03:26 -0500

    feat(logging): imporve format of logger

--- vidly/startup/logger.js
+++ vidly/startup/logger.js
@@ -1,26 +1,44 @@
 const winston = require("winston");
 require("express-async-errors");
 
+// Common log format for files
+const fileFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.printf(({ timestamp, level, message }) => {
+    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+  })
+);
+
+// Console format with colors
+const consoleFormat = winston.format.combine(
+  winston.format.colorize(), // colorizes the level
+  winston.format.timestamp(),
+  winston.format.printf(({ timestamp, level, message }) => {
+    // if message is an object, stringify it
+    const msg =
+      typeof message === "object" ? JSON.stringify(message, null, 2) : message;
+    return `${timestamp} [${level}]: ${msg}`;
+  })
+);
+
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-    })
-  ),
   exitOnError: false,
   transports: [
-    new winston.transports.File({ filename: "logfile.log", level: 'error' }),
-    new winston.transports.Console({ level: 'info' }),
+    new winston.transports.File({
+      filename: "logfile.log",
+      level: "error",
+      format: fileFormat,
+    }),
+    new winston.transports.Console({ level: "info", format: consoleFormat }),
   ],
   exceptionHandlers: [
-    new winston.transports.File({ filename: "exceptions.log", level: 'error' }),
-    new winston.transports.Console({ level: 'error' }),
+    new winston.transports.File({ filename: "exceptions.log", level: "error" }),
+    new winston.transports.Console({ level: "error", format: consoleFormat }),
   ],
   rejectionHandlers: [
-    new winston.transports.File({ filename: "rejections.log", level: 'error' }),
-    new winston.transports.Console({ level: 'error' }),
+    new winston.transports.File({ filename: "rejections.log", level: "error" }),
+    new winston.transports.Console({ level: "error", format: consoleFormat }),
   ],
 });
 

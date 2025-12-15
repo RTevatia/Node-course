7bf40b9b79eda88f2c257095ebcf3358c870288b
Rahul <rtevatia2@gmail.com>
2025-12-15 05:55:42 -0500

    test(lib): add unit tests for absolute function

--- Writing_Test/testing-demo/tests/lib.test.js
+++ Writing_Test/testing-demo/tests/lib.test.js
@@ -1,3 +1,16 @@
-test("our first test", () => {
-    throw new Error('Something failed.')
+const lib = require("../lib");
+
+test("absolute - should return positive if number is positive", () => {
+  const result = lib.absolute(1);
+  expect(result).toBe(1);
+});
+
+test("absolute - should return positive if number is negative", () => {
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
+
+test("absolute - should return 0 if number is 0", () => {
+  const result = lib.absolute(0);
+  expect(result).toBe(0);
 });

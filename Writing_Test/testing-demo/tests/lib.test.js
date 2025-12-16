edf8b3d3fd499b9a11b8f387d52edade80cc8eca
Rahul <rtevatia2@gmail.com>
2025-12-16 06:29:24 -0500

    test(notify): mock db and mail interactions
    Verifies notifyCustomer behavior without
    calling real database or email services.

--- Writing_Test/testing-demo/tests/lib.test.js
+++ Writing_Test/testing-demo/tests/lib.test.js
@@ -1,5 +1,6 @@
 const lib = require("../lib");
 const db = require("../db");
+const mail = require("../mail");
 
 describe("absolute", () => {
   it("should return positive if number is positive", () => {
@@ -76,8 +77,6 @@ describe("registerUser", () => {
   });
 });
 
-
-
 describe("applyDiscount", () => {
   it("should apply 10% discount if customer has more than 10 points", () => {
     db.getCustomerSync = function (customerId) {
@@ -88,5 +87,21 @@ describe("applyDiscount", () => {
     const order = { customerId: 1, totalPrice: 10 };
     lib.applyDiscount(order);
     expect(order.totalPrice).toBe(9);
+  });
+});
+
+describe("notifyCustomer", () => {
+  it("should send an email to the customer", () => {
+    db.getCustomerSync = function (customerId) {
+      return { email: "a" };
+    };
+
+    let mailSent = false;
+    mail.send = function (to, subject) {
+      mailSent = true;
+    };
+
+    lib.notifyCustomer({ customerId: 1 });
+    expect(mailSent).toBe(true);
   });
 });

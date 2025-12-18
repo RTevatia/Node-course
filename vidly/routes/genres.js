645457b352b0fa8029acbf9335c099ae407da967
Rahul <rtevatia2@gmail.com>
2025-12-18 06:42:28 -0500

    fix(routes): return 404 for invalid genre id

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -13,6 +13,9 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id))
+    return res.status(404).send("Invalid ID");
+
   const genre = await Genre.findById(req.params.id);
   if (!genre)
     return res.status(404).send("The genre with given ID was not found");
--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -36,7 +36,12 @@ describe("/api/genre", () => {
       const res = await request(server).get("/api/genre/" + genre._id);
 
       expect(res.status).toBe(200);
-      expect(res.body).toHaveProperty('name', genre.name);
+      expect(res.body).toHaveProperty("name", genre.name);
+    });
+
+    it("should return 404 if invalid id is passed", async () => {
+      const res = await request(server).get("/api/genre/1");
+      expect(res.status).toBe(404);
     });
   });
 });

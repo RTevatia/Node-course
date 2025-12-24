7c5020cddf32f0550a6b80bcc102a98e6a5126ef
Rahul <rtevatia2@gmail.com>
2025-12-24 08:05:36 -0500

    refactor(rentals): add static lookup method

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -2,17 +2,14 @@ const Joi = require("joi");
 const JoiObjectId = require("joi-objectid");
 const moment = require("moment");
 const auth = require("../middleware/auth");
-const validate = require('../middleware/validate');
+const validate = require("../middleware/validate");
 const { Rental } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const express = require("express");
 const router = express.Router();
 
 router.post("/", [auth, validate(validateReturn)], async (req, res) => {
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental) return res.status(404).send("Rental was not found.");
 
